821ba7c3ce5b2399ee668ef2538deea6d85dec2b
samiksha-kumari <samiksha-kumari_outlook.com>
2019-11-02 20:03:10 +0530

    Updates

---
+++ precache-manifest.e74e51a96ca254ebb2d7b548a99bb037.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f7be9d61b6ca0dcdd6d65dd34e3a632",
+    "url": "/coding-assignment/index.html"
+  },
+  {
+    "revision": "a7838c84f67eed97244e",
+    "url": "/coding-assignment/static/css/main.bff03ec7.chunk.css"
+  },
+  {
+    "revision": "392ec0094854dd7427b5",
+    "url": "/coding-assignment/static/js/2.4b6a914e.chunk.js"
+  },
+  {
+    "revision": "a7838c84f67eed97244e",
+    "url": "/coding-assignment/static/js/main.31e39a5f.chunk.js"
+  },
+  {
+    "revision": "c55ec1aa15af713b9c8d",
+    "url": "/coding-assignment/static/js/runtime-main.e5a31aae.js"
+  },
+  {
+    "revision": "bd80794fddbffb4031fab537cc898680",
+    "url": "/coding-assignment/static/media/man.bd80794f.png"
+  }
+]);
